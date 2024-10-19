050dc515d954f1481d3fa5e7dc96aac456f8c149
VitorSilva <vitorviniciusdev@gmail.com>
2024-10-19 15:10:58 -0300

    Aula 3: Escrevendo entidades de negocio

--- src/DevIO.Business/Models/Fornecedores/Endereco.cs
+++ src/DevIO.Business/Models/Fornecedores/Endereco.cs
@@ -4,5 +4,16 @@ namespace DevIO.Business.Models.Fornecedores
 {
     internal class Endereco : Entity
     {
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemnto { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+        //EF Relations 
+        public Fornecedor Fornecedor { get; set; }
+
     }
 }
