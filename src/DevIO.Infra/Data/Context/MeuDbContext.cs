f54a0daddc74d3b1ff54e3ce624e2fd3e2b24c92
VitorSilva <vitorviniciusdev@gmail.com>
2024-10-21 09:09:06 -0300

    Aula 7: Otimizando a modelagem via contexto

--- src/DevIO.Infra/Data/Context/MeuDbContext.cs
+++ src/DevIO.Infra/Data/Context/MeuDbContext.cs
@@ -2,6 +2,7 @@
 using DevIO.Business.Models.Produtos;
 using DevIO.Infra.Data.Mappings;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace DevIO.Infra.Data.Context
 {
@@ -9,7 +10,8 @@ namespace DevIO.Infra.Data.Context
     {
         public MeuDbContext() : base("DefaultConnection")
         {
-
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
         }
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
@@ -18,10 +20,19 @@ namespace DevIO.Infra.Data.Context
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
 
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar")
+                .HasMaxLength(100));
+
             modelBuilder.Configurations.Add(new FornecedorConfig());
             modelBuilder.Configurations.Add(new EnderecoConfig());
             modelBuilder.Configurations.Add(new ProdutoConfig());
 
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
