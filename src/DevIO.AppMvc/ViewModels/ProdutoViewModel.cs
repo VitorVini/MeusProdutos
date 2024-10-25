3b5b55b93c7beea4ce2e5dede9a9d77225eaf60b
VitorSilva <vitorviniciusdev@gmail.com>
2024-10-25 15:40:16 -0300

    Aula 18, 19 e 20: Scaffolding + Automapper + Controller

--- src/DevIO.AppMvc/ViewModels/ProdutoViewModel.cs
+++ src/DevIO.AppMvc/ViewModels/ProdutoViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Web;
 
 namespace DevIO.AppMvc.ViewModels
 {
@@ -29,8 +27,8 @@ namespace DevIO.AppMvc.ViewModels
         [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
 
-        [DisplayName("Imagem do Produto")]
-        public HttpPostedFileBase ImagemUpload { get; set; }
+        //[DisplayName("Imagem do Produto")]
+        //public HttpPostedFileBase ImagemUpload { get; set; }
         public string Imagem { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
@@ -41,8 +39,8 @@ namespace DevIO.AppMvc.ViewModels
 
         [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
-        public FornecedorViewModel Fornecedor { get; set; }
+        //public FornecedorViewModel Fornecedor { get; set; }
 
-        public IEnumerable<FornecedorViewModel> Fornecedores { get; set; }
+        //public IEnumerable<FornecedorViewModel> Fornecedores { get; set; }
     }
 }
